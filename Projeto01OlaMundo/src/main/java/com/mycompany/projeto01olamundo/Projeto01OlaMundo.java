fe1f6eb253bd8328550c6a62c515d13976a17b5b
Brusilv17 <54293168+Brusilv17@users.noreply.github.com>
2019-08-19 19:41:40 -0300

    Projeto Alo Mundo

---
+++ Projeto01OlaMundo/src/main/java/com/mycompany/projeto01olamundo/Projeto01OlaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projeto01olamundo;
+
+/**
+ *
+ * @author mathe
+ */
+public class Projeto01OlaMundo {
+    public static void main(String[] args){
+        System.out.println("Olá");
+        
+    }
+}
